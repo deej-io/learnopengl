77d0d91994e00c469437c54731b629fc2fe0ae04
Daniel J. Rollins <daniel@djrollins.com>
2021-05-13 21:55:03 +0100

    render two triangles, 2 VAOs

--- src/main.cpp
+++ src/main.cpp
@@ -51,10 +51,7 @@ int main() {
         return 1;
     }
 
-    auto on_exit = scope_exit([]{
-        fmt::print(fmt::fg(fmt::color::green), "Closing down GLFW");
-        glfwTerminate();
-    });
+    auto on_exit = scope_exit(&glfwTerminate);
 
     glfwSetErrorCallback(&error_handler);
 
@@ -121,39 +118,37 @@ int main() {
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
 
-    auto vertices = std::array<float, 18> {
+    auto vertices_1 = std::array<float, 18> {
         -0.9f, -0.9f, 0.0f,
         -0.1f, -0.9f, 0.0f,
         -0.5 ,  0.9f, 0.0f,
+    };
+
+    auto vertices_2 = std::array<float, 18> {
          0.1f, -0.9f, 0.0f,
          0.9f, -0.9f, 0.0f,
          0.5 ,  0.9f, 0.0f
     };
 
-    auto indices = std::array {
-        0, 1, 2,
-        3, 4, 5
-    };
+    unsigned int VAOs[2], VBOs[2];
 
-    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(2, VAOs);
+    glGenBuffers(2, VBOs);
 
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices.data(), GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices.data(), GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glBindVertexArray(VAOs[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_1), vertices_1.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
     glEnableVertexAttribArray(0);
 
-    while(!glfwWindowShouldClose(window.get()))
-    {
+    glBindVertexArray(VAOs[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_2), vertices_2.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glEnableVertexAttribArray(0);
+
+    while(!glfwWindowShouldClose(window.get())) {
         if (glfwGetKey(window.get(), GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(window.get(), true);
         }
@@ -162,8 +157,10 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shader_program);
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glBindVertexArray(VAOs[0]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAOs[1]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window.get());
         glfwPollEvents();
