c4f275a45a941710f74d829d97e8eed113f6edba
Daniel J. Rollins <daniel@djrollins.com>
2021-05-16 17:51:59 +0100

    add clang format

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,35 @@
+#include <GLFW/glfw3.h>
+#include <fmt/color.h>
+#include <fmt/format.h>
+#include <glad/glad.h>
+
 #include <experimental/memory>
 
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
-#include "fmt/format.h"
-#include "fmt/color.h"
-
-using std::experimental::observer_ptr;
 using std::experimental::make_observer;
+using std::experimental::observer_ptr;
 
-template <typename Func>
+template<typename Func>
 struct [[nodiscard]] scope_exit {
-    scope_exit(Func&& func) : func_(std::forward<Func>(func)) {}
-    ~scope_exit() { func_(); }
+    scope_exit(Func&& func)
+    : func_(std::forward<Func>(func)) {}
+    ~scope_exit() {
+        func_();
+    }
+
 private:
     Func func_;
 };
 
-template <typename Func> scope_exit(Func &&) -> scope_exit<Func>;
+template<typename Func>
+scope_exit(Func&&) -> scope_exit<Func>;
 
-[[noreturn]] static void error_handler(int error_code, const char *message) {
+[[noreturn]] static void error_handler(int error_code, const char* message) {
     fmt::print(stderr, fmt::fg(fmt::color::red), FMT_STRING("Error ({0}): {1}"), error_code, message);
     glfwTerminate();
     std::exit(error_code);
 }
 
-static const char *vertex_shader_source = R"(
+static const char* vertex_shader_source = R"(
 #version 330 core
 
 layout (location = 0) in vec3 position;
@@ -34,7 +39,7 @@ void main() {
 }
 )";
 
-static const char *fragment_shader_source = R"(
+static const char* fragment_shader_source = R"(
 #version 330 core
 
 out vec4 FragColor;
@@ -45,13 +50,12 @@ void main() {
 )";
 
 int main() {
-
     if (!glfwInit()) {
         fmt::print(stderr, fmt::fg(fmt::color::red), "Failed to initialize GLFW");
         return 1;
     }
 
-    auto on_exit = scope_exit([]{
+    auto on_exit = scope_exit([] {
         fmt::print(fmt::fg(fmt::color::green), "Closing down GLFW");
         glfwTerminate();
     });
@@ -67,14 +71,13 @@ int main() {
 
     glfwMakeContextCurrent(window.get());
 
-    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
-    {
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
         fmt::print(stderr, fmt::fg(fmt::color::red), "Failed to initialize GLAD");
         return -1;
     }
 
     glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window.get(), [](GLFWwindow *, int width, int height) {
+    glfwSetFramebufferSizeCallback(window.get(), [](GLFWwindow*, int width, int height) {
         glViewport(0, 0, width, height);
     });
 
@@ -111,7 +114,7 @@ int main() {
     glLinkProgram(shader_program);
 
     glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
-    if(!success) {
+    if (!success) {
         std::array<char, 512> info;
         glGetProgramInfoLog(shader_program, info.size(), nullptr, info.data());
         fmt::print(stderr, fmt::fg(fmt::color::red), FMT_STRING("ERROR::SHADER::PROGRAM::LINKING_FAILED\n{}"), info.data());
@@ -121,15 +124,13 @@ int main() {
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
 
-    auto vertices = std::array {
-         0.0f,  0.5f, 0.0f,  // top top
-        -0.5f, -0.5f, 0.0f,  // bottom left
-         0.5f, -0.5f, 0.0f,  // bottom right
+    auto vertices = std::array{
+        0.0f, 0.5f, 0.0f, // top top
+        -0.5f, -0.5f, 0.0f, // bottom left
+        0.5f, -0.5f, 0.0f, // bottom right
     };
 
-    auto indices = std::array {
-        0, 1, 2,
-    };
+    auto indices = std::array{ 0, 1, 2 };
 
     unsigned int VAO, VBO, EBO;
 
@@ -148,9 +149,7 @@ int main() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
 
-
-    while(!glfwWindowShouldClose(window.get()))
-    {
+    while (!glfwWindowShouldClose(window.get())) {
         if (glfwGetKey(window.get(), GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(window.get(), true);
         }
